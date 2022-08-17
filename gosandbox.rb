193f4270fd1f873fbfaf4aeffa957e1a9cf81578
josephedward <josephedwardwork@gmail.com>
2022-08-17 12:27:24 -0400

    Brew formula update for gosandbox version v0.1.2

--- gosandbox.rb
+++ gosandbox.rb
@@ -5,20 +5,20 @@
 class Gosandbox < Formula
   desc ""
   homepage "https://github.com/josephedward/gosandbox"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/josephedward/gosandbox/releases/download/v0.1.1/gosandbox_0.1.1_Darwin_x86_64.tar.gz"
-      sha256 "ca4dcc3d4adec292aba69610fdb68dcf9ea19c3da5473f7f048b99fa0b0ac656"
+      url "https://github.com/josephedward/gosandbox/releases/download/v0.1.2/gosandbox_0.1.2_Darwin_x86_64.tar.gz"
+      sha256 "bb3cafbc4f6a480207c3c3b2da8734f9851ee3295e0c452717435f6244c066b0"
 
       def install
         bin.install "gosandbox"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/josephedward/gosandbox/releases/download/v0.1.1/gosandbox_0.1.1_Darwin_arm64.tar.gz"
-      sha256 "7fa87dceed229afe3e7a83eaf28a58db20fae8302928efb34a681ccc8c7fa5e3"
+      url "https://github.com/josephedward/gosandbox/releases/download/v0.1.2/gosandbox_0.1.2_Darwin_arm64.tar.gz"
+      sha256 "6f04a246d1eef33ff4ca6ac75585ef4bd2d1210e154e0511e7156e4a85d3e56a"
 
       def install
         bin.install "gosandbox"
@@ -28,16 +28,16 @@ class Gosandbox < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/josephedward/gosandbox/releases/download/v0.1.1/gosandbox_0.1.1_Linux_x86_64.tar.gz"
-      sha256 "9ca86ce3cae911d73b4a8b026f5b9864b1f768f0e1f4afffbeee863c9d14f858"
+      url "https://github.com/josephedward/gosandbox/releases/download/v0.1.2/gosandbox_0.1.2_Linux_x86_64.tar.gz"
+      sha256 "94fdda2095522af4f1410680af0863ab52ce99fd4ac924204baf777eb9150cac"
 
       def install
         bin.install "gosandbox"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/josephedward/gosandbox/releases/download/v0.1.1/gosandbox_0.1.1_Linux_arm64.tar.gz"
-      sha256 "5c70ed7c256ded8e31b171474744471bdf89fbb98b962d3c561f4984ed1200d2"
+      url "https://github.com/josephedward/gosandbox/releases/download/v0.1.2/gosandbox_0.1.2_Linux_arm64.tar.gz"
+      sha256 "8f1efd4aade9139578ab1668a7c1ad9ccafee461c60ef3492e8e17463100147e"
 
       def install
         bin.install "gosandbox"
